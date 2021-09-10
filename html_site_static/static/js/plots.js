a4db1f21b80f031c29db5f5405dd5dbd62336ee0
Lucy Green <lucygreen@Lucys-MacBook-Pro.local>
2021-09-10 10:47:32 -0400

    building the js file for proof of concept to have a js site for hottest cities

--- html_site_static/static/js/plots.js
+++ html_site_static/static/js/plots.js
@@ -1,17 +1,46 @@
 function init() {
     var selector = d3.select("#selDataset");
-    d3.json("https://raw.githubusercontent.com/sjpozzuoli/Daves_Eagles/lucy_green/SQL%20Data%20Join/realtor_mortgage.json").then((data) => {
-      console.log(data);
-      var cityNames = data.city_name;
-      console.log("cityNames", cityNames);
-      cityNames.forEach((sample) => {
-        selector
-          .append("option")
-          .text(sample)
-          .property("value", sample);
-      }); 
+    d3.csv("https://raw.githubusercontent.com/sjpozzuoli/Daves_Eagles/lucy_green/SQL%20Data%20Join/realtor_mortgage.csv").then(function(data) {
+      //console.log(data);
+      data.forEach(function(d){
+          //create number values from strings
+          d.demand_score = +d.demand_score;
+          d.hotness_rank = +d.hotness_rank;
+          d.hotness_rank_yy = +d.hotness_rank_yy;
+          d.ldp_unique_viewers_per_property_vs_us =+ d.ldp_unique_viewers_per_property_vs_us;
+          d.ldp_unique_viewers_per_property_yy =+ d.ldp_unique_viewers_per_property_yy;
+          d.median_days_on_market_yy =+ d.median_days_on_market_yy ;
+          d.median_listing_price_yy =+ d.median_listing_price_yy;
+          d.mortgage_rate =+ d.mortgage_rate;
+          d.nielsen_rank =+ d.nielsen_rank;
+          d.supply_score =+ d.supply_score;
+          d.date = new Date(d.date);
+          
+      
+        console.log(data[0]);
+        var cityNames = d.city_name;
+            console.log("cityNames", cityNames);
+        var state = d.state_abb;
+            console.log("state", state);
+        var city_state = cityNames + ", " + state;
+            console.log(city_state);
+    
+      
+
+      data.forEach((d) => {
+  
+
+            selector
+                .append("optionChanged")
+                .text(city_state)
+                .property("value", city_state)
+        })
+
+      })
+
+    }); 
    
-  })}
+  }
   
  // function buildMetadata(city_name) {
  //   d3.json("https://raw.githubusercontent.com/sjpozzuoli/Daves_Eagles/lucy_green/SQL%20Data%20Join/realtor_mortgage.json").then((data) => {
